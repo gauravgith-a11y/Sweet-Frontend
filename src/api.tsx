83262992f10b411dada448248ebec8c5ec1a160d
gauravgith-a11y <gauravgith@gmail.com>
2025-11-09 13:58:12 +0530

    APPI

--- src/api.tsx
+++ src/api.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://sweetshopbackend.infinityfreeapp.com/api" // Local testing (no proxy)
-      : "/api/proxy/api", // ✅ Use Vercel proxy in production
+      ? "https://sweetshopbackend.infinityfreeapp.com/api" // for local testing
+      : "/api/proxy/api", // ✅ for Vercel proxy in production
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
